5e00b290da094af9991089d2bc971ba70f7b0a61
kiran <kiran.cs.aiet@gmail.com>
2023-01-18 17:32:35 +0530

    first commit

---
+++ js/myjQuery.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    console.log($, 'testing');
+$('#click1').click();
+$('#click1').click(function () {
+    console.log('you clicked on appoitment');
+    // $(this).hide();
+})
+
+// scroll animation code
+
+
+});
+console.log('i made new page')
